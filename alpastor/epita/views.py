137ba4bbf131e4115f9bc64246b5b000ce84ac88
Chris LeMatty <clematty@gmail.com>
2018-03-20 14:31:47 +0100

    Fixed custom urls for attendance module

--- alpastor/epita/views.py
+++ alpastor/epita/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Student, Professor, StudentCourse, Course, Attendance, Schedule
@@ -12,37 +12,10 @@ def home(request):
 class ScheduleList(ListView):
     model = Schedule
 
-    # def get_queryset(self):
-    #     self.course_id = get_object_or_404(Schedule, name=self.kwargs['course_id'])
-    #     # return Book.objects.filter(course_id=self.course_id)
-    #     pass
 
 class AttendanceList(ListView):
     model = Attendance
 
-
-# class AttendanceStudentClass(ListView):
-#     template_name = 'epita/schedule_list.html'
-#
-#     def get_queryset(self):
-#         self.course_id = get_object_or_404(Attendance, schedule_id=self.kwargs['course_id'])
-#         return Schedule.objects.filter(course_id=self.course_id)
-
-
-# class AttendanceSemesterList(ListView):
-#     model = Attendance
-#     template_name = "epita/attendance_list.html"
-#
-#     def get_queryset(self):
-#         # semester_val =
-#         # print(Course.objects.filter(semester=))
-#         return Course.objects.filter(semester=self.kwargs['semester'])
-
-# class AttendanceStudentClass(ListView):
-#     template_name = 'epita/schedule_list.html'
-#
-#     def get_queryset(self):
-#         return "This worked"
 
 def people(request):
 
