834fc590d16102091207efd94dc12a97d9fda490
Chris LeMatty <clematty@gmail.com>
2018-03-31 15:38:32 +0200

    Added tests for template & status code for attendance & course views

--- alpastor/epita/tests/tests_models.py
+++ alpastor/epita/tests/tests_models.py
@@ -1,13 +1,9 @@
 from django.test import TestCase
 from django.test import Client
-from epita.models import Student
-from epita.models import Course
-from epita.models import StudentCourse
-from epita.models import Professor
-from epita.models import Room
-from epita.models import Schedule
+from epita.models import Student, Professor, Course, Room, Schedule, Attendance, StudentCourse
 from accounts.models import User
-from epita.models import Attendance
+from django.urls import reverse
+
 import datetime
 
 '''
@@ -16,7 +12,7 @@ Model Tests
 Below are our tests for the database objects, making sure that tables fit together properly
     
 '''
-class StudentCourseTest(TestCase):
+class AttendanceTest(TestCase):
     def setUp(self):
 
         User.objects.create(first_name="fn0", last_name="ln0", external_email="external0@gmail.com", password="abc",
@@ -83,12 +79,12 @@ class StudentCourseTest(TestCase):
         # Create 20 instances of a class schedule for course0
         for i in range(0,20):
             Schedule.objects.create(course_id=course0, date=datetime.date.today()+datetime.timedelta(days=i),
-                                    start_time="10:00", end_time="11:00", room_id=room0)
+                                    start_time="10:00:00", end_time="11:00:00", room_id=room0)
 
         # Create 10 instances of a class schedule for course1
         for i in range(0,10):
             Schedule.objects.create(course_id=course1, date=datetime.date.today()+datetime.timedelta(days=i),
-                                    start_time="10:00", end_time="11:00", room_id=room1)
+                                    start_time="10:00:00", end_time="11:00:00", room_id=room1)
 
         schedule0 = Schedule.objects.create(course_id=course1, date=datetime.date.today(), start_time="10:00:00",
                                             end_time="13:00:00", room_id=room0)
@@ -101,6 +97,7 @@ class StudentCourseTest(TestCase):
 
         Attendance.objects.create(student_id=student0, schedule_id=schedule0, status=1)
         Attendance.objects.create(student_id=student1, schedule_id=schedule1, status=2)
+
         for i in range(20):
             Attendance.objects.create(student_id=student2, schedule_id=schedule2, status=3)
 
@@ -108,7 +105,6 @@ class StudentCourseTest(TestCase):
         student0_courses = StudentCourse.objects.filter(student_id__user__first_name__exact="fn0").count()
         student1_courses = StudentCourse.objects.filter(student_id__user__first_name__exact="fn1").count()
         student2_courses = StudentCourse.objects.filter(student_id__user__first_name__exact="fn2").count()
-
         self.assertEqual(student0_courses, 2)
         self.assertEqual(student1_courses, 1)
         self.assertEqual(student2_courses, 1)
@@ -116,13 +112,22 @@ class StudentCourseTest(TestCase):
     def test_models_schedule_by_course(self):
         course0_schedules = Schedule.objects.filter(course_id__title__exact="Sample Course 0").count()
         course1_schedules = Schedule.objects.filter(course_id__title__exact="Sample Course 1").count()
-
         self.assertEqual(course0_schedules, 20)
         self.assertEqual(course1_schedules, 12)
 
     def test_models_attendance_by_schedule(self):
         schedule0_attendance = Attendance.objects.filter(schedule_id__course_id__title__exact="Sample Course 1").count()
         schedule1_attendance = Attendance.objects.filter(schedule_id__course_id__title__exact="Sample Course 2").count()
-
         self.assertEqual(schedule0_attendance, 2)
         self.assertEqual(schedule1_attendance, 20)
+
+    def test_course_view_status_code_and_template(self):
+        self.response = self.client.get(reverse('course_list'))
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'epita/course_list.html')
+
+    def test_attendance_view_status_code_and_template(self):
+        url = reverse('attendance_list') + '?schedule_id=1'
+        self.response = self.client.get(url, follow=True)
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'epita/attendance_list.html')
