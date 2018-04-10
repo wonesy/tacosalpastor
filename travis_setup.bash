7df987e9b1ce6477ed97269f1deb8042d169bb8f
Cameron Jones <camandrewjones@gmail.com>
2018-04-10 10:42:29 +0200

    Attempting travis fix 5

--- travis_setup.bash
+++ travis_setup.bash
@@ -4,10 +4,26 @@ export MYSQL_DATABASE=alpastor
 export MYSQL_USERNAME=alpastoradmin
 export MYSQL_USER_PASSWORD=alpastorpass
 
-sudo mysql -e 'CREATE DATABASE IF NOT EXISTS alpastor;'
-sudo mysql -e "CREATE USER 'alpastoradmin'@'localhost' IDENTIFIED BY 'alpastorpass';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'alpastoradmin'@'localhost';"
-sudo mysql -e "FLUSH PRIVILEGES;"
+rc=$(sudo mysql -e 'CREATE DATABASE IF NOT EXISTS alpastor;')
+if [[ ${rc} != 0 ]]; then
+    echo "Create database failed"
+fi
+
+rc=$(sudo mysql -e "CREATE USER 'alpastoradmin'@'localhost' IDENTIFIED BY 'alpastorpass';")
+if [[ ${rc} != 0 ]]; then
+    echo "Create user failed"
+fi
+
+rc=$(sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'alpastoradmin'@'localhost';")
+if [[ ${rc} != 0 ]]; then
+    echo "Granting privileges failed"
+fi
+
+rc=$(sudo mysql -e "FLUSH PRIVILEGES;")
+if [[ ${rc} != 0 ]]; then
+    echo "Flushing privileges failed"
+fi
+
 
 # Migrations nonsense
 
