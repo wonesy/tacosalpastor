0567a4d3d29c846a14d2ea7cd0b3833cbae9e66b
Chris LeMatty <clematty@gmail.com>
2018-04-18 14:31:36 +0200

    Fixed post to update existing objects for attendance

--- alpastor/epita/forms.py
+++ alpastor/epita/forms.py
@@ -5,9 +5,8 @@ class AttendanceForm(forms.ModelForm):
 
     class Meta:
         model = Attendance
-        fields = ['student_id', 'status', 'file_upload']
+        fields = ['id', 'schedule_id', 'student_id', 'status', 'file_upload']
         widgets = {
             'status' : forms.RadioSelect(choices=Attendance.ATTENDANCE_CHOICES),
-            'schedule_id' : forms.HiddenInput(),
-            'upload_time' : forms.HiddenInput()
+            'upload_time' : forms.HiddenInput(),
         }
