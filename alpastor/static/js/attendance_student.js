2b940d49e3f6711811d6e6c6f69dadcc71efd97d
Chris LeMatty <clematty@gmail.com>
2018-06-26 12:42:03 +0200

    Fixed file upload duplication

--- alpastor/static/js/attendance_student.js
+++ alpastor/static/js/attendance_student.js
@@ -1,9 +1,11 @@
 function addFileUploadButton() {
-    let divName = document.getElementById('fileUpload');
-    let addInput = document.createElement('INPUT');
-    addInput.setAttribute('type', 'file');
-    addInput.setAttribute('name', 'file_upload');
-    addInput.setAttribute('id', 'id_file_upload');
-    divName.appendChild(addInput);
-    console.log(divName);
+    if (document.getElementById('id_file_upload') === null) {
+        let divName = document.getElementById('fileUpload');
+        let addInput = document.createElement('INPUT');
+        addInput.setAttribute('type', 'file');
+        addInput.setAttribute('name', 'file_upload');
+        addInput.setAttribute('id', 'id_file_upload');
+        divName.appendChild(addInput);
+        console.log(divName);
+    }
 }
