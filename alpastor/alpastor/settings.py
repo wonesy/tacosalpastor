277c9722a39751a2d26a365bde5d1824b4a4dfbc
Cameron Jones <camandrewjones@gmail.com>
2018-07-05 16:43:28 +0200

    Secrets

--- alpastor/alpastor/settings.py
+++ alpastor/alpastor/settings.py
@@ -16,12 +16,11 @@ import logging
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zhl88=^=iuuw=_j+^9^hoeu(5!s28lh45p!k6vjqy-6l)9z1)j'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -225,6 +224,6 @@ LOGGING = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.office365.com'
-EMAIL_HOST_USER = 'jones.cameron@epita.fr'
-EMAIL_HOST_PASSWORD = 'oh<V,vox'
+EMAIL_HOST_USER = os.getenv('ADMIN_EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('ADMIN_EMAIL_PASSWORD')
 EMAIL_PORT = 587
