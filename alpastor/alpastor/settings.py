8d3bee0ca4bbdf0a8fbeb94ccf30cd1f0b221656
Cameron Jones <camandrewjones@gmail.com>
2018-07-12 11:42:17 +0200

    Fixing media URL

--- alpastor/alpastor/settings.py
+++ alpastor/alpastor/settings.py
@@ -238,6 +238,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
+AWS_MEDIA_LOCATION = 'static'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
 STATICFILES_DIRS = [
@@ -245,3 +246,5 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_MEDIA_LOCATION)
