849fea69c5d91882e394bb54fb07f324ca9a9571
Cameron Jones <camandrewjones@gmail.com>
2018-07-12 11:17:02 +0200

    AWS media storage

--- alpastor/alpastor/settings.py
+++ alpastor/alpastor/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import logging
+from alpastor.aws.conf import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_countries',
+    'storages',
     'epita',
     'accounts',
     'quiz',
